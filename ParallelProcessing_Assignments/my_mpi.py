058a12e2ff317ba0d339bb23bbe0b0a539eaed37
Nick <aresr4@gmail.com>
2019-04-16 01:09:42 +0300

    mpi refactored

--- ParallelProcessing_Assignments/my_mpi.py
+++ ParallelProcessing_Assignments/my_mpi.py
@@ -1,33 +1,40 @@
+import time
+import numpy as np
 from mpi4py import MPI
 
 
-size = MPI.COMM_WORLD.Get_size()
-if size != 2:
-    print("Ping-pong only works with 2 ranks")
-    exit(1)
+def load_func(x, pow=5.):
+    arr = 0
+    for y in x:
+        arr += y ** pow
+    return arr
 
-comm = MPI.COMM_WORLD
-rank = MPI.COMM_WORLD.Get_rank()
-#
-# Play ping-pong between ranks 0 and 1
-#
-PING = 0
-PONG = 1
-MESSAGE_ID = 5  # arbitrary unique choice
-counter = 0
-for i in range(10):
-    # PING sends to PONG and waits to receive a return message
-    if rank == PING:
-        print("PING sending %d to PONG" % counter)
-        comm.send(counter, dest=PONG, tag=MESSAGE_ID)
-        counter = comm.recv(source=PONG, tag=MESSAGE_ID)
-        print("PING receiving %d from PONG" % counter)
-    # PONG waits for PING's message then returns it, incremented
-    elif rank == PONG:
-        counter = comm.recv(source=PING, tag=MESSAGE_ID)
-        print("PONG receiving %d from PING and incrementing" % counter)
-        counter += 1
-        print("PONG sending %d to PING" % counter)
-        comm.send(counter, dest=PING, tag=MESSAGE_ID)
-if rank == PING:
-    print("Total length of rally: %d times there and back" % counter)
+
+def compute(sequence_length=10000000):
+    # generating data
+    x = np.random.randn(sequence_length)
+
+    # single pass
+    start = time.time()
+    res = load_func(x)
+    stop = time.time()
+    
+    # MPI block below, NUM is number of nodes
+    # can be run with mpiexec -n NUM python mpiProg.py or ./mpi_launch.sh NUM
+
+    '''
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    i = len(x) // size
+    buf = x[i * rank: i * (rank + 1)]
+    arr = load_func(buf)
+    res = comm.allreduce(arr)
+    '''
+
+    print(f"Single pass time is {stop - start}")
+    print(f"Computed result: {res}")
+
+
+if __name__ == '__main__':
+    compute(10000000)
