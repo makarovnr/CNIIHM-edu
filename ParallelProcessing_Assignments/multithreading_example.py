84a39886b535021a4a361bdae0e9c5e0e624032c
Nick <aresr4@gmail.com>
2019-04-15 18:57:46 +0300

    code updated

--- ParallelProcessing_Assignments/multithreading_example.py
+++ ParallelProcessing_Assignments/multithreading_example.py
@@ -1,5 +1,4 @@
 import time
-
 from threading import Thread
 
 
@@ -7,24 +6,31 @@ total_sleep = 0
 
 
 class MyThread(Thread):
-    def __init__(self, name):
+    thread_list = []
+
+    def __init__(self, name, am):
         Thread.__init__(self)
         self.name = name
+        self.amount = am
 
     def run(self):
         global total_sleep
-        amount = 10
+        amount = self.amount
         total_sleep += amount
         time.sleep(amount)
         msg = "%s is running" % self.name
         print(msg)
+        MyThread.thread_list.append(self)
 
 
-def create_threads():
-    for i in range(5):
+def create_threads(treads_num=5, total_wait=15.):
+    for i in range(treads_num):
         name = "Thread #%s" % (i + 1)
-        my_thread = MyThread(name)
+        my_thread = MyThread(name, total_wait / treads_num)
         my_thread.start()
+
+    for thread in MyThread.thread_list:
+        thread.join()
 
 
 if __name__ == "__main__":
--- ParallelProcessing_Assignments/my_mpi.py
+++ ParallelProcessing_Assignments/my_mpi.py
@@ -1,18 +1,33 @@
-import numpy as np
-import threading
+from mpi4py import MPI
 
 
-def single():
-    pass
+size = MPI.COMM_WORLD.Get_size()
+if size != 2:
+    print("Ping-pong only works with 2 ranks")
+    exit(1)
 
-
-def parallel():
-    pass
-
-
-def run():
-    pass
-
-
-if __name__ == '__main__':
-    run()
+comm = MPI.COMM_WORLD
+rank = MPI.COMM_WORLD.Get_rank()
+#
+# Play ping-pong between ranks 0 and 1
+#
+PING = 0
+PONG = 1
+MESSAGE_ID = 5  # arbitrary unique choice
+counter = 0
+for i in range(10):
+    # PING sends to PONG and waits to receive a return message
+    if rank == PING:
+        print("PING sending %d to PONG" % counter)
+        comm.send(counter, dest=PONG, tag=MESSAGE_ID)
+        counter = comm.recv(source=PONG, tag=MESSAGE_ID)
+        print("PING receiving %d from PONG" % counter)
+    # PONG waits for PING's message then returns it, incremented
+    elif rank == PONG:
+        counter = comm.recv(source=PING, tag=MESSAGE_ID)
+        print("PONG receiving %d from PING and incrementing" % counter)
+        counter += 1
+        print("PONG sending %d to PING" % counter)
+        comm.send(counter, dest=PING, tag=MESSAGE_ID)
+if rank == PING:
+    print("Total length of rally: %d times there and back" % counter)
